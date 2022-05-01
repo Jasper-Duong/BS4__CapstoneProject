9e1a6756f6176e554b98c897002fd226205ae891
Jasper-Duong <duongngochung13@gmail.com>
2022-05-01 12:58:18 +0700

    Finished responsive #count & #video

--- main.js
+++ main.js
@@ -22,24 +22,33 @@ function switchClass(id, classRemove, classAdd) {
 function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
-getEle("menuToggler").onclick = function () {
-  var classList = getEle("navbarNav").classList;
-  if (classList[classList.length - 1] === "animate__zoomOut") {
-    switchClass("navbarNav", "animate__zoomOut", "animate__zoomIn");
-  }
-  getEle("navbarNav").style.display = "block";
-};
+function showDiv(div, btn) {
+  getEle(btn).onclick = function () {
+    var classList = getEle(div).classList;
+    if (classList[classList.length - 1] === "animate__zoomOut") {
+      switchClass(div, "animate__zoomOut", "animate__zoomIn");
+    }
+    getEle(div).style.display = "block";
+  };
+}
+showDiv("navbarNav", "menuToggler");
 //Close button animation & Disapperance
-getEle("closeBtn").onclick = function () {
-  switchClass("navbarNav", "animate__zoomIn", "animate__zoomOut");
-  delay(300).then(() => (getEle("navbarNav").style.display = "none"));
-};
+function closeDiv(div, btn) {
+  getEle(btn).onclick = function () {
+    switchClass(div, "animate__zoomIn", "animate__zoomOut");
+    delay(300).then(() => (getEle(div).style.display = "none"));
+  };
+}
+closeDiv("navbarNav", "closeNavBtn");
 //BackToTop
 document.onscroll = function () {
   if (window.scrollY > 100) {
-    getEle('backToTop').classList.add('animate__fadeIn');
+    getEle("backToTop").classList.add("animate__fadeIn");
     getEle("backToTop").style.display = "block";
   } else {
     getEle("backToTop").style.display = "none";
   }
 };
+//Play video popup
+showDiv("videoClip", "playVid")
+closeDiv("videoClip", "closeVideoBtn");
